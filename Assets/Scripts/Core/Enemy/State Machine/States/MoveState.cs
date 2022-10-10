118538f07c4442492ed8e118278d9b2524eb4e05
Krivushkina Daria <daridakr@gmail.com>
2022-10-10 19:09:01 +0300

    Added Enemy Rise State

--- Assets/Scripts/Core/Enemy/State Machine/States/MoveState.cs
+++ Assets/Scripts/Core/Enemy/State Machine/States/MoveState.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class MoveState : State
 {
     [SerializeField] private float _speed;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _animator.Play("Walk");
+    }
 
     private void Update()
     {
